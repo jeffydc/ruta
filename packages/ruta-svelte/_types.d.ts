d6f69eceb033d6789fe98d349cfe81210cb2b6f3
jeffydc.xyz <vcs@jeffydc.xyz>
2026-01-31 18:16:43 +0800

    chore: remove extra files

--- packages/ruta-svelte/_types.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Component } from 'svelte';
-
-declare module '@jeffydc/ruta-core' {
-	export interface Register {
-		component: Component;
-	}
-}
--- packages/ruta-vue/_types.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Component, DefineComponent } from 'vue';
-
-declare module '@jeffydc/ruta-core' {
-	export interface Register {
-		component: Component | DefineComponent;
-	}
-}
